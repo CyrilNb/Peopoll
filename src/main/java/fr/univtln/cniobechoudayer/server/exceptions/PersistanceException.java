9dbd181e055b3dcc5997ae13928156a401d33de6
Cyril <cyril.niobe@gmail.com>
2017-10-21 17:44:15 +0200

    ADD custom exceptions

---
+++ src/main/java/fr/univtln/cniobechoudayer/server/exceptions/PersistanceException.java
@@ -0,0 +1,21 @@
+package fr.univtln.cniobechoudayer.server.exceptions;
+
+/**
+ * Cette classe représente les erreurs émises lors de la persistance.
+ * L'erreur d'origine est accessible via getException().
+ */
+public class PersistanceException extends Exception {
+    private Exception exception;
+
+    public PersistanceException(String message) {
+        super(message);
+    }
+
+    public PersistanceException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+}
