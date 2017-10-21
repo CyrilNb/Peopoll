3e956399327023d8dc4871f4d9f1d02fc8f87cd0
Cyril <cyril.niobe@gmail.com>
2017-10-21 11:39:09 +0200

    ADDS implementation of Client and Server class

---
+++ src/main/java/fr/univtln/cniobechoudayer/client/MainClient.java
@@ -0,0 +1,18 @@
+package fr.univtln.cniobechoudayer.client;
+
+/**
+ * MainClient class which contain the main method called by the execution of the client
+ * Created by Cyril on 21/10/2017.
+ */
+public class MainClient {
+
+    /**
+     * main method
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //Start UI and connect to the server by telnet (TCP/IP) here
+
+    }
+}
