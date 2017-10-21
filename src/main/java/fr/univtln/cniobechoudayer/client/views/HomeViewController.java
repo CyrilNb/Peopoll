a1582b9ff74f4910d577e30800ae371c815b8c91
Cyril <cyril.niobe@gmail.com>
2017-10-21 13:27:30 +0200

    UPDATE HomeView cardpane from Gluon to javafx native component

--- src/main/java/fr/univtln/cniobechoudayer/client/views/HomeViewController.java
+++ src/main/java/fr/univtln/cniobechoudayer/client/views/HomeViewController.java
@@ -1,7 +1,8 @@
 package fr.univtln.cniobechoudayer.client.views;
 
 /**
+ * Class which represents the home
  * Created by Cyril on 16/10/2017.
  */
-public class HomeView {
+public class HomeViewController {
 }
