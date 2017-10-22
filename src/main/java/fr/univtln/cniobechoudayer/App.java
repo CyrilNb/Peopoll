3ed41b0c772ce5a30edd0e6c18df8e50ef7bb03b
houdayec <houdayer.corentin@gmail.com>
2017-10-22 11:27:58 +0200

    ADD resources folder with fxml views inside

--- src/main/java/fr/univtln/cniobechoudayer/App.java
+++ src/main/java/fr/univtln/cniobechoudayer/App.java
@@ -1,5 +1,10 @@
 package fr.univtln.cniobechoudayer;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PatternLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +14,7 @@ import java.lang.invoke.MethodHandles;
 /**
  * Hello world!
  */
-public class App {
+public class App extends Application{
     @SuppressWarnings("unused")
     private static final Class[] shadeHack = {org.apache.log4j.RollingFileAppender.class,
             org.apache.log4j.ConsoleAppender.class,
@@ -19,8 +24,25 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         logger.info("App started.");
         logger.debug("About to talk :");
         System.out.println("Hello world !");
+
+        /*
+        Call and display the homeView
+         */
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/HomeView.fxml"));
+
+        Scene scene = new Scene(root, 700, 400);
+
+        primaryStage.setTitle("Peopoll");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
