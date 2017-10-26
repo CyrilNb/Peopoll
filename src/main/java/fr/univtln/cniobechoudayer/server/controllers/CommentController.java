9b6e25efae2cd2dee152edd14ca9a531ca0c1a09
Cyril <cyril.niobe@gmail.com>
2017-10-26 15:49:54 +0200

    ADD implementation of CommentController

--- src/main/java/fr/univtln/cniobechoudayer/server/controllers/CommentController.java
+++ src/main/java/fr/univtln/cniobechoudayer/server/controllers/CommentController.java
@@ -1,4 +1,35 @@
 package fr.univtln.cniobechoudayer.server.controllers;
 
+import fr.univtln.cniobechoudayer.model.EntityManager;
+import fr.univtln.cniobechoudayer.model.entities.Comment;
+import fr.univtln.cniobechoudayer.server.exceptions.PersistanceException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 public class CommentController {
+    public static List<Comment> getAllCommentsByPoll(int idPoll) throws PersistanceException {
+        EntityManager entityManager = EntityManager.getInstance();
+        List<Comment> listReturnedComments;
+        try{
+            listReturnedComments = Comment.findAll(idPoll);
+            if(listReturnedComments != null){
+                return listReturnedComments;
+            }else {
+                return Collections.emptyList();
+            }
+        }catch (PersistanceException e){
+            e.getException().printStackTrace();
+            return null;
+        }finally {
+            entityManager.dispose();
+        }
+    }
+
+    public static void createCommentInDB(String nameAuthor, String content, Date dateComment, int idPoll) throws PersistanceException{
+        EntityManager entityManager = EntityManager.getInstance();
+        Comment comment = new Comment(nameAuthor,content,dateComment,idPoll);
+        entityManager.persist(comment);
+        entityManager.dispose();
+    }
 }
